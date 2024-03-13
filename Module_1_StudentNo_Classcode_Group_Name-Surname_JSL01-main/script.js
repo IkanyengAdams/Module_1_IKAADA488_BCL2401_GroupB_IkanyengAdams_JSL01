79b0f977a2f272bb975afa7d90392914a6f808a3
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-03-13 14:17:24 +0200

    Updated Js file

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -6,7 +6,7 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+            document.getElementById('result').innerText = result;uuuuuuuuuuuuuuu
 }
 
 
