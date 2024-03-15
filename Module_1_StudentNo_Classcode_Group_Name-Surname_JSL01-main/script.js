5993f18fdf9d0ca4afc309e959149ccb2f632974
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-03-15 09:22:55 +0200

    Added red and green colors

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -3,9 +3,13 @@ function validateSyntax() {
     let result = /^pet_\d{4}[A-Za-z]+$/; // Placeholder for validation result
 
         if (result.test(input)) {
-            document.getElementById('result').innerText = "Valid Syntax"; 
+            document.getElementById('result').innerText = "Valid Syntax";
+            document.getElementById('result').classList.remove("invalid");
+            document.getElementById('result').classList.add("valid"); 
         } else {
             document.getElementById('result').innerText = "Invalid Syntax";
+            document.getElementById('result').classList.remove("valid");
+            document.getElementById('result').classList.add("invalid");
     }
             
 }
